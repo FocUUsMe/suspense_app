c2c6d82feb8e7dc481dc9abddd614c4e1518a27a
FocUUsMe <timoxxastolarov@gmail.com>
2026-02-16 22:16:00 +0300

    'pageUpd'

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,53 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import { Jersey_10 } from "next/font/google";
+
+const jersey_10 = Jersey_10({
+  subsets: ['latin'],
+  weight: ['400']
+});
 
 export default function Home() {
+  const [ navItems, setNavItems ] = useState([
+    {
+      id: 1,
+      title: 'For you'
+    },
+    {
+      id: 2,
+      title: 'Profile'
+    },
+    {
+      id: 3,
+      title: 'Notifications'
+    },
+    {
+      id: 4,
+      title: 'Search'
+    },
+    {
+      id: 5,
+      title: 'Support'
+    }
+  ]);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       
+        <nav className={styles.nav_bar}>
+          { navItems.map((item, index) => {
+              return <div key={item.id} className={jersey_10.className}>
+                {item.title}
+              </div>
+          })}
+        </nav>
+
+        <section className={styles.main_body}>
+
+        </section>
       </main>
     </div>
   );
